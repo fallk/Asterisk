edf6203de58381967510d0097a691c23faf5fb03
uwx <cadrekucra@gmx.com>
2018-09-11 15:41:20 -0300

    initial commit

---
+++ Asterisk/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Asterisk
+{
+    internal class Program
+    {
+        private const string Asterisk =
+                @"          !
+         ""#
+         $%
+         &'
+         ()
+         *+
+         ,-
+         ./
+         01
+23456789:;<=>?@ABCDEF
+GHIJKLMNOPQRSTUVWXYZ[
+        \]^_
+       `a  bc
+      de    fg
+     hi      jk
+    lm        no
+   pq          rs
+  tu            vw
+ xy              z{
+|}                ~";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Asterisk);
+        }
+    }
+}
